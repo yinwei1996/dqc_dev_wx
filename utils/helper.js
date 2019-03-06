abea2390b1cc639279e8e4d202355061c391b1c6
JerryZhang <junz1314@hotmail.com>
2019-03-07 00:03:09 +0800

    #API指向测试服务器；

--- utils/helper.js
+++ utils/helper.js
@@ -16,7 +16,7 @@ __cfg = {
   host: 'https://dqctest.yunxiaoming.cn/',
   // 开发版
   //host: 'http://localbox.hecai360.com/',
-  host: 'http://localhost/',
+  //host: 'http://localhost/',
   header: { 'content-type': 'application/x-www-form-urlencoded;charset=utf-8' }
 },
 /* ------------------------------
